69142fbb2794bc2e2311496d01f355f1e87c3ddb
Griffin McLennan <mclennangriffin@gmail.com>
2022-05-06 17:56:00 -0700

    feat: setup react-number-format

--- components/CollateralRow/index.tsx
+++ components/CollateralRow/index.tsx
@@ -1,5 +1,6 @@
-import { Button, Flex } from "@chakra-ui/react";
+import { Text, Flex, Input } from "@chakra-ui/react";
 import { Collateral } from "../../data/collaterals";
+import NumberFormat from "react-number-format";
 
 interface Props {
     collateral: Collateral;
@@ -9,9 +10,21 @@ interface Props {
 
 const CollateralRow = ({ collateral, amount, onChange }: Props) => {
     return (
-        <Flex>
-            {collateral.symbol}, {amount}
-            <Button onClick={() => onChange(collateral.symbol, amount + 100)}>Click me</Button>
+        <Flex alignItems="center" my="10px" width="200px">
+            <Text width="100px" mr="15px">
+                {collateral.symbol}
+            </Text>
+            <NumberFormat
+                customInput={Input}
+                // variant="unstyled"
+                value={amount}
+                thousandSeparator
+                onValueChange={(values) => {
+                    const { floatValue } = values;
+                    onChange(collateral.symbol, floatValue ?? 0);
+                }}
+            />
+            {/* <Button onClick={() => onChange(collateral.symbol, amount + 100)}>Click me</Button> */}
         </Flex>
     );
 };
